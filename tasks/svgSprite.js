ab286489bf291fb5145ba02af7bfd7124db5401b
silverblue <shit09875@gmail.com>
2023-12-27 21:27:38 +0600

    sprite

--- tasks/svgSprite.js
+++ tasks/svgSprite.js
@@ -4,13 +4,12 @@ const { src, dest } = gulp
 import svgSprites from 'gulp-svg-sprite'
 
 export const svgSprite = () => {
-	return src('src/assets/img/**/*.svg', {})
+	return src('src/assets/img/svg/**/*.svg', {})
 		.pipe(
 			svgSprites({
 				mode: {
 					stack: {
-						sprite: '../icons/icons.svg',
-						example: true
+						sprite: '../icons/icons.svg'
 					}
 				}
 			})
